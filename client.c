1ec02f0e656df2c00f91f7c14279d07412bd6326
Adam Sheehan <a.t.sheehan@gmail.com>
2011-02-24 21:41:36 -0500

    Added basic TCP networking code for client and server.

--- client.c
+++ client.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define DEFAULT_PORT 4485
+#define BUFFER_SIZE 100
 
 int main() {
-  printf("i am a client.\n");
+  int sd, port, result;
+  struct sockaddr_in server_addr;
+  const char *server = "localhost";
 
+  port = DEFAULT_PORT;
+
+  sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sd < 0) {
+    fprintf(stderr, "error: unable to open a socket\n");
+    exit(1);
+  }
+
+  memset(&server_addr, 0, sizeof(struct sockaddr_in));
+  server_addr.sin_family      = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server_addr.sin_port	      = htons(port);
+
+  result = connect(sd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
+  if (result < 0) {
+    fprintf(stderr, "error: unable to connect to server\n");
+    close(sd);
+    exit(1);
+  }
+
+  char buffer[BUFFER_SIZE];
+  size_t bytes_received;
+
+  bytes_received = recv(sd, buffer, BUFFER_SIZE, 0);
+  if (bytes_received > 0) {
+    printf("received: %s\n", buffer);
+  }
+
+  close(sd);
+  
   return 0;
 }
--- server.c
+++ server.c
@@ -1,7 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define DEFAULT_PORT 4485
+#define MAX_PENDING 10
+#define BUFFER_SIZE 20
 
 int main() {
-  printf("i am a server.\n");
+  int sd, port, result;
+  struct sockaddr_in server_addr;
+  int client_sd, send_buffer_size;
+  char buffer[BUFFER_SIZE];
+  size_t client_size, bytes_sent;
 
+  port = DEFAULT_PORT;
+
+  sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (sd < 0) {
+    fprintf(stderr, "error: unable to open a socket\n");
+    exit(1);
+  }
+
+  memset(&server_addr, 0, sizeof(struct sockaddr_in));
+  server_addr.sin_family      = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port	      = htons(port);
+
+  result = bind(sd, (struct sockaddr *)&server_addr, 
+		sizeof(struct sockaddr_in));
+  if (result < 0) {
+    fprintf(stderr, "error: unable to bind\n");
+    close(sd);
+    exit(1);
+  }
+
+  result = listen(sd, MAX_PENDING);
+  if (result < 0) {
+    fprintf(stderr, "error: could not listen\n");
+    close(sd);
+    exit(1);
+  }
+
+  strncpy(buffer, "hello, world!\n", BUFFER_SIZE);
+  send_buffer_size = strlen(buffer) + 1;
+
+  while (1) {
+    client_sd = accept(sd, 0, 0);
+    if (client_sd < 0) {
+      fprintf(stderr, "error: could not accept client connection\n");
+      close(sd);
+      exit(1);
+    }
+
+    bytes_sent = send(client_sd, buffer, send_buffer_size, 0);
+    if (bytes_sent != send_buffer_size) {
+      fprintf(stderr, "error: could not send full packet\n");
+      close(client_sd);
+      close(sd);
+      exit(1);
+    }
+
+    close(client_sd);
+  }
+  close(sd);
   return 0;
 }
